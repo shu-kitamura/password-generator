8e6ba57454fe455d97736c32544b065b61837580
shu-kitamura <shusei3316@yahoo.co.jp>
2025-08-03 15:29:36 +0900

    メイン処理を実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::Parser;
+use rand::Rng;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = 16)]
+    length: usize,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789)(*&^%$#@!~";
+
+    let mut rng = rand::thread_rng();
+    let password: String = (0..args.length)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+
+    println!("{password}");
 }
